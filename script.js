9ac9f8dd49d32cf62dc88024eff1b86df994171e
Zaid Jamil <zeejaydev@gmail.com>
2022-03-22 00:32:39 -0600

    more than half way done push

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const button = document.getElementById('floating-btn')
 
-
 const addMovie = ()=>{
     //getting elements from the dom
     const inputVal = document.querySelector('input').value
@@ -37,11 +36,15 @@ const addMovie = ()=>{
     del.appendChild(delIcon)
     buttonsContainer.appendChild(del)
 
+    
+
     if(!inputVal){
         alert('Please type a movie name first')
     }else{
+        //hide the middle text
         text.classList.add('hide')
         
+        //start the api call
         fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=cc340213&t=${inputVal}`)
         .then( resp => resp.json() )
         .then( results => {
@@ -53,46 +56,65 @@ const addMovie = ()=>{
             const releaseDate = document.createElement('h3')
             releaseDate.textContent = `Release Date: ${results.Released}`
 
+            //creating the star ratings
+            const starContainer = document.createElement('div')
+            starContainer.className = 'star-container' 
+            const starIcon = document.createElement('i')
+            starIcon.className = 'fa-solid'
+            starIcon.classList.add('fa-star')
+            const halfStarIcon = document.createElement('i')
+            halfStarIcon.className = 'fa-solid'
+            halfStarIcon.classList.add('fa-star-half-stroke')
+
             if(results.Error){
                 alert(results.Error)
-                text.classList.remove('hide')
+                //check to not show the text if there are movies alreay
+                const lists = document.querySelectorAll('.list')
+                if(lists.length > 0){
+                    text.classList.add('hide')
+                }else{
+                    text.classList.remove('hide')
+                }
+                
                 
             }else{
                 //poster div
-                // const poster  = document.createElement('img')
-                // poster.src = 'https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg'
                 posterDiv.appendChild(poster)
 
                 //movieinfo div
                 movieInfoDiv.appendChild(movieTitle)
                 movieInfoDiv.appendChild(releaseDate)
+                let rating = results.imdbRating / 2
+                for(i=1;i<rating;i++){
+                    starContainer.appendChild(starIcon.cloneNode(true))
+                    if(rating % 2 !== 0){
+                        starContainer.appendChild(halfStarIcon)
+                    }
+                }
+                movieInfoDiv.appendChild(starContainer)
                 movieInfoDiv.appendChild(buttonsContainer)
 
                 container.appendChild(listDiv)
                 listDiv.appendChild(posterDiv)
                 listDiv.appendChild(movieInfoDiv)
 
+                //selecting all delete buttons to start listening to clicks
+                const deleteBtns = document.querySelectorAll('.del-btn')
+                //adding an event listener on each button
+                deleteBtns.forEach( btn => btn.addEventListener('click',deleteMovie))
+                
             }
         })
         .catch(e=>console.log(e))
-
-       
-        //poster div
-        // const poster  = document.createElement('img')
-        // poster.src = 'https://m.media-amazon.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg'
-        // posterDiv.appendChild(poster)
-
-        //movieinfo div
-        // movieInfoDiv.appendChild(movieTitle)
-        // movieInfoDiv.appendChild(buttonsContainer)
-
-        // container.appendChild(listDiv)
-        // listDiv.appendChild(posterDiv)
-        // listDiv.appendChild(movieInfoDiv) 
 
         document.querySelector('input').value=""
 
     }
 }
-
+//listing to clicks on the floating button
 button.addEventListener('click', addMovie)
+
+const deleteMovie = (event)=>{
+    //getting offsetParent to get the main div where the button lives
+    event.target.offsetParent.remove()
+}
